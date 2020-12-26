d6f1b6c90d408b6275ba6e7aa045ff1426f0a872
mehmetemmidev <mehmetemmidev@gmail>
2020-12-26 17:36:39 -0500

    first commit, check environment

---
+++ src/test/java/com/json/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.json;
+
+import java.util.Map;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        Map<String, String> env = System.getenv();
+        for (String envName : env.keySet()) {
+            System.out.format("%s=%s%n",
+                    envName,
+                    env.get(envName));
+        }
+    }
+}
